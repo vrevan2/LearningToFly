fde68c18625efe1d524f9eb52993017dff9f1657
Sai Phaltankar <saiphaltankar@gmail.com>
2018-03-05 20:06:11 -0600

    added plots to app.R

--- Application/app.R
+++ Application/app.R
@@ -67,6 +67,10 @@ df$ArrMonthofYear <- month(df$ArrDateTime)
 #Airlines Lookup
 airlines <- read.csv("data/airlines.csv")
 
+month = 1
+Airport1 = 'ORD'
+Airport2 = 'MDW'
+
 #### End of Global Variables Creation Section ####
 
 
@@ -177,6 +181,36 @@ common15OriginAPort2 <- merge(x = common15OriginAPort2, y = airports[,c('Airport
 common15OriginAPort2 <- common15OriginAPort2[c(1,4,2,3)]
 
 combineCommonOriginAirports <- rbind(common15OriginAPort1, common15OriginAPort2)
+
+#### Arrivals and departures for hour of the day across a month
+dfMonth<-df[df$Month == month,]
+
+dfMonthHoD<- dfMonth[dfMonth$Dest == Airport1 | dfMonth$Origin == Airport1| dfMonth$Dest == Airport2 | dfMonth$Origin == Airport2,]
+dfMonthHoD<-dfMonthHoD[,c('ArrHourofDay', 'DepHourofDay', 'Dest', 'Origin')]
+dfMonthHoD$airport <- ''
+dfMonthHoD<- na.omit(dfMonthHoD)
+dfMonthHoD[dfMonthHoD$Origin == Airport1 | dfMonthHoD$Dest == Airport1,]$airport <- 1
+dfMonthHoD[dfMonthHoD$Origin == Airport2 | dfMonthHoD$Dest == Airport2,]$airport <- 2
+dfMonthHoD<-data.frame('Arrivals' = dfMonthHoD$ArrHourofDay, 'Departures' = dfMonthHoD$DepHourofDay, 'airport' = dfMonthHoD$airport)
+dfMonthHoDMelt<-melt(dfMonthHoD, id='airport')
+dfMonthHoDMelt$airport <- ifelse(dfMonthHoDMelt$airport == 1, Airport1, Airport2)
+
+### number of arrivals and departures for day of the week across a month
+dfMonthDoW<- dfMonth[dfMonth$Dest == Airport1 | dfMonth$Origin == Airport1| dfMonth$Dest == Airport2 | dfMonth$Origin == Airport2,]
+dfMonthDoW <- dfMonthDoW[,c('DayOfWeek', 'Dest', 'Origin')]
+colnames(dfMonthDoW)<-c('Day', 'Arr', 'Dep')
+dfMonthDoW$Arr<-sapply(dfMonthDoW$Arr, function(x) if(is.na(x)) x=0 else if(x == Airport1) x=1 else if (x==Airport2) x=2 else x=0)
+dfMonthDoW$Dep<-sapply(dfMonthDoW$Dep, function(x) if(is.na(x)) x=0 else if(x == Airport1) x=1 else if (x==Airport2) x=2 else x=0)
+dfMonthDoWMelt <- melt(dfMonthDoW, id='Day')
+dfMonthDoWMelt<-dfMonthDoWMelt[!((dfMonthDoWMelt$variable == 'Arr' & dfMonthDoWMelt$value==0) | (dfMonthDoWMelt$variable == 'Dep' & dfMonthDoWMelt$value==0)),]
+dfMonthDoWMelt$cat<-''
+dfMonthDoWMelt[dfMonthDoWMelt$value == 1,]$cat <- Airport1
+dfMonthDoWMelt[dfMonthDoWMelt$value == 2,]$cat <- Airport2
+
+### number of delays for hour of the day across a month
+
+dfdelay<-dfMonth[(dfMonth$Dest == Airport1 | dfMonth$Dest == Airport2) & dfMonth$ArrDelay > 0,]
+dfdelay<-dfdelay[,c('Dest','ArrDelay', 'CRSArrHourofDay')]
 
 
 ############## layout ################
@@ -359,6 +393,25 @@ server <- function(input, output){
     caption = 'Most common 15 Destination Airports for Midway International Airport',
     options = list(paging = FALSE, searching = FALSE, dom = 't', searching = FALSE,order = list(list(1,'asc')),columnDefs = list(list(visible = FALSE, targets = c(0,4)))))
   )
+  
+  # arrivals and departures by hour of the day
+  output$bar_hour<- renderPlot({
+    ggplot(dfMonthHoDMelt, aes(x = airport, fill = variable)) + 
+      geom_bar(stat = 'count', position = 'stack') + facet_grid(~ value) + 
+      scale_fill_manual(labels=c('Arrivals', 'Departures'), values = c('#7a8da8', '#c6adaa')) +
+      labs(x='Hour of the Day', y='Count')
+  })
+  
+  # arrivals and departures by day of the week
+  output$bar_day<- renderPlot({
+    ggplot(dfMonthDoWMelt, aes(x = cat, fill = variable)) + geom_bar(stat = 'count', position = 'stack') + facet_grid(~ Day) + scale_fill_manual(labels=c('Arrivals', 'Departures'), values = c('#7a8da8', '#c6adaa')) +
+      labs(x='Day of the Week', y='Count')
+  })
+  
+  output$bar_delay<- renderPlot({
+    ggplot(dfdelay, aes(x = Dest, fill = Dest)) + geom_bar(stat = 'count') + facet_grid(~ CRSArrHourofDay) + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank())+
+      labs(x='Hour of the Day', y='Count')
+  })
   
   # displaying text for the about section
   output$about_project <- renderUI({
